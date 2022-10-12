f066504955220446f6a1b0faee3cf6c4b76e406e
serjCookies <locobu@icloud.com>
2022-10-12 19:07:26 +0400

    alternative 15-2 from book solutions

--- TRY IT YOURSELF/15-2 Plotting a Simple Line Graph/15-2. Colored Cubes/scatter_cubes.py
+++ TRY IT YOURSELF/15-2 Plotting a Simple Line Graph/15-2. Colored Cubes/scatter_cubes.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-x_values = range(1, 5001)
+x_values = list(range(5001))
 y_values = [x**3 for x in x_values]
 
 plt.style.use('seaborn-v0_8')
@@ -11,17 +11,17 @@ ax.scatter(
     cmap=plt.cm.spring, s=10)
 
 # set chart title and label axes
-ax.set_title('Cube Numbers', fontsize=20)
+ax.set_title('Cubes', fontsize=20)
 ax.set_xlabel('Value', fontsize=14)
 ax.set_ylabel('Cube of Value', fontsize=14)
 
 # set size of tick labels
-ax.tick_params(axis='both', which='major', labelsize=9)
+ax.tick_params(axis='both', labelsize=9)
 
 # prevent scientific notation (1_000_000 instead of 1e6)
 plt.ticklabel_format(style='plain')
 
 # set the range for each axis
-ax.axis([0, 5100, 0, 130_000_000_000])
+ax.axis([0, 5100, 0, 5100**3])
 
 plt.show()
