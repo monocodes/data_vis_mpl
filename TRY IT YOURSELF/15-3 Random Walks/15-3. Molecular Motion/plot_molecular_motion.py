fd87ce45e766d4673ca7ebe9b16729ffba38215f
serjCookies <locobu@icloud.com>
2022-10-17 16:58:34 +0400

    used zorder to see start and end dots clearer

--- TRY IT YOURSELF/15-3 Random Walks/15-3. Molecular Motion/plot_molecular_motion.py
+++ TRY IT YOURSELF/15-3 Random Walks/15-3. Molecular Motion/plot_molecular_motion.py
@@ -17,10 +17,10 @@ while True:
     plt.style.use('classic')
     fig, ax = plt.subplots(figsize=(15, 9))
     point_numbers = range(rw.num_points)
-    ax.plot(rw.x_values, rw.y_values, linewidth=1)
+    ax.plot(rw.x_values, rw.y_values, linewidth=1, zorder=1)
     
     # Emphasize the first and last points.
-    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100, zorder=2)
     ax.scatter(
         rw.x_values[-1], rw.y_values[-1],
         c='red', edgecolors='none', s=100)
