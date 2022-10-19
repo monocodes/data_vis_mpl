ac5dbbfa8c4ae4d5cb181ca21d6b5a26dbd73bce
serjCookies <locobu@icloud.com>
2022-10-19 16:57:27 +0400

    added task description

--- TRY IT YOURSELF/15-3 Random Walks/15-5. Refactoring/rw_visual.py
+++ TRY IT YOURSELF/15-3 Random Walks/15-5. Refactoring/rw_visual.py
@@ -1,3 +1,13 @@
+### 15-5. Refactoring ###
+"""
+The fill_walk() method is lengthy. Create a new method called get_step() to determine the direction and distance for each step, and then calculate the step. You should end up with two calls to get_step() in fill_walk():
+
+x_step = self.get_step()
+y_step = self.get_step()
+
+This refactoring should reduce the size of fill_walk() and make the method easier to read and understand.
+"""
+
 import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
